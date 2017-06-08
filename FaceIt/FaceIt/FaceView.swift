c1a9f3c2c8c734f06f32c7862ed539cb0a0f636d
smitchell-gt <steven.mitchell.tkd@gmail.com>
2017-06-08 13:01:29 -0500

    [smitchell-gt | Lecture 4] Improves FaceView to draw a blue circle

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -11,7 +11,12 @@ import UIKit
 class FaceView: UIView {
 
     override func draw(_ rect: CGRect) {
-        
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        UIColor.blue.set()
+        path.stroke()
     }
 
 }
