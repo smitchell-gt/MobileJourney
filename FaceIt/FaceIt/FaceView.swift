8ba9e7d47735aa6d7cbee189032c6b4774b38177
smitchell-gt <steven.mitchell.tkd@gmail.com>
2017-06-08 12:03:53 -0500

    [smitchell-gt] Initializing FaceIt demo app and adding custom FaceView to storyboard

---
+++ FaceIt/FaceIt/FaceView.swift
@@ -0,0 +1,17 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Steven Mitchell on 6/8/17.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}
---
+++ FaceIt/FaceIt/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Steven Mitchell on 6/8/17.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+}
+
