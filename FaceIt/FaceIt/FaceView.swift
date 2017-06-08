c468c0ece0ebebfb84c8c459e2747b0b80b7787c
smitchell-gt <steven.mitchell.tkd@gmail.com>
2017-06-08 13:09:41 -0500

    [smitchell-gt | Lecture 4] Refactors FaceView to extract simple properties and functionality

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class FaceView: UIView {
 
+    private var skullRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2
+    }
+    
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
     override func draw(_ rect: CGRect) {
-        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
-        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = 5.0
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
     }
 
+    private func pathForSkull() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = 5.0
+        return path
+    }
+    
 }
