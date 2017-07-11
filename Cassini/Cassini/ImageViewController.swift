0748f7eb62a3c1c854ffbefcc2b61039d66832ce
smitchell-gt <steven.mitchell.tkd@gmail.com>
2017-07-11 14:35:12 -0500

    [Lecture 7] Builds out ScrollView example

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -1,15 +1,65 @@
-//
-//  ImageViewController.swift
-//  Cassini
-//
-//  Created by Steven Mitchell on 7/7/17.
-//  Copyright © 2017 ThoughtWorks. All rights reserved.
-//
-
 import UIKit
 
 class ImageViewController: UIViewController {
-
     
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.3
+            scrollView.maximumZoomScale = 1.0
+            scrollView.contentSize = imageView.frame.size
+            scrollView.addSubview(imageView)
+        }
+    }
 
+    var imageUrl: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+
+    fileprivate var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageUrl = URL(string: "https://cdn6.bigcommerce.com/s-hgcm9g/product_images/theme_images/03__48132.jpg")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageUrl {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+}
+
+extension ImageViewController : UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
