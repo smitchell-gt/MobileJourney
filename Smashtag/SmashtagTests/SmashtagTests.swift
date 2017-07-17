333bb2c0305c144a1d21c01a7c9a020fe357f6c8
smitchell-gt <steven.mitchell.tkd@gmail.com>
2017-07-17 09:27:14 -0500

    [Lecture 9] Creates Smashtag project, adds Fastlane, Alamofire, Quick/Nimble

---
+++ Smashtag/SmashtagTests/SmashtagTests.swift
@@ -0,0 +1,14 @@
+import Quick
+import Nimble
+
+class SmashtagTests: QuickSpec {
+    override func spec() {
+        describe("SmashtagTests") {
+            context("when this condition is met") {
+                it("does this thing") {
+                    
+                }
+            }
+        }
+    }
+}
